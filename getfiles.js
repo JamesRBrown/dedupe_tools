8e86ba57681bc647abe3a54cc2ed13d39729ec40
Server <generaldelta@yahoo.com>
2018-08-12 10:56:01 -0600

    added ability to pass in arguments from CLI

--- getfiles.js
+++ getfiles.js
@@ -1,189 +1,219 @@
 (function(){
-    var fs = require('fs');
-    var p = require("path");
-    var sqlite3 = require('better-sqlite3');
     
-    var initRun = true;
-    var db = new sqlite3('files.sqlite');
+    var arg1 = process.argv[2];
+    var arg2 = process.argv[3];
     
-    var processCount = 0;
+    if(arg1 === '-h' || arg1 === '--help'){
+        var help = '';
+        
+        help += "\n";
+        help += "getfiles is a file analysis tool\n\n";
+        
+        help += "It will walk a given path and record all\n";
+        help += "file meta data to a specified sqlite file.\n\n";
+        
+        help += "The first argument is the path to walk.\n";
+        help += "The second argument is the destination sqlite file.\n\n";
+        
+        help += "If the sqlite file doesn't exist, it will be created.\n";
+        help += "Otherwise an existing sqlite file will be updated.\n\n";
+        
+        help += "Usage example: node getfiles.js ./ test.sqlite\n";
+        
+        
+        console.log(help);
+    }else if(arg1 && arg2){
+        (function(walkpath, dbname){    
     
-    function mapWalkPath(path, mapFunction){
-        path = path || '.';
-        mapFunction = mapFunction || function(){};
-        try{
-            fs.readdir(path, function(err, items) {
-                if(items){
-                    items.forEach(function(item){
-                        if(item && fs.existsSync(`${path}/${item}`) && !exception(item)){
-                            var ap = p.resolve(`${path}/${item}`);
-                                var pd = p.resolve(path);
-                                (function(ap, path, item){
-                                    try{
-                                        fs.stat(ap, function(err, stats){
-                                            if(stats && stats.isDirectory()){
-                                                //console.log(ap+"/");
-                                                mapWalkPath(ap, mapFunction);
-                                            }else if(stats){
-                                                //mapFunction(ap);
-                                                mapFunction({
-                                                    file: ap,
-                                                    filename: item,
-                                                    path: path,
-                                                    dev: stats.dev,
-                                                    mode: stats.mode,
-                                                    nlink: stats.nlink,
-                                                    uid: stats.uid,
-                                                    gid: stats.gid,
-                                                    rdev: stats.rdev,
-                                                    blksize: stats.blksize,
-                                                    inodeID: stats.ino,
-                                                    size: stats.size,
-                                                    blocks: stats.blocks,
-                                                    atimeMs: stats.atimeMs,
-                                                    mtimeMs: stats.mtimeMs,
-                                                    ctimeMs: stats.ctimeMs,
-                                                    birthtimeMs: stats.birthtimeMs,
-                                                    atime: stats.atime,
-                                                    mtime: stats.mtime,
-                                                    ctime: stats.ctime,
-                                                    birthtime: stats.birthtime
+            var fs = require('fs');
+            var p = require("path");
+            var sqlite3 = require('better-sqlite3');
+
+            var initRun = true;
+            var db = new sqlite3(dbname);
+
+            var processCount = 0;
+
+            function mapWalkPath(path, mapFunction){
+                path = path || '.';
+                mapFunction = mapFunction || function(){};
+                try{
+                    fs.readdir(path, function(err, items) {
+                        if(items){
+                            items.forEach(function(item){
+                                if(item && fs.existsSync(`${path}/${item}`) && !exception(item)){
+                                    var ap = p.resolve(`${path}/${item}`);
+                                        var pd = p.resolve(path);
+                                        (function(ap, path, item){
+                                            try{
+                                                fs.stat(ap, function(err, stats){
+                                                    if(stats && stats.isDirectory()){
+                                                        //console.log(ap+"/");
+                                                        mapWalkPath(ap, mapFunction);
+                                                    }else if(stats){
+                                                        //mapFunction(ap);
+                                                        mapFunction({
+                                                            file: ap,
+                                                            filename: item,
+                                                            path: path,
+                                                            dev: stats.dev,
+                                                            mode: stats.mode,
+                                                            nlink: stats.nlink,
+                                                            uid: stats.uid,
+                                                            gid: stats.gid,
+                                                            rdev: stats.rdev,
+                                                            blksize: stats.blksize,
+                                                            inodeID: stats.ino,
+                                                            size: stats.size,
+                                                            blocks: stats.blocks,
+                                                            atimeMs: stats.atimeMs,
+                                                            mtimeMs: stats.mtimeMs,
+                                                            ctimeMs: stats.ctimeMs,
+                                                            birthtimeMs: stats.birthtimeMs,
+                                                            atime: stats.atime,
+                                                            mtime: stats.mtime,
+                                                            ctime: stats.ctime,
+                                                            birthtime: stats.birthtime
+                                                        });
+                                                    }
+                                                    //console.log(err);
                                                 });
                                             }
-                                            //console.log(err);
-                                        });
-                                    }
-                                    catch(err){
+                                            catch(err){
 
-                                    }
-                                })(ap, pd, item);
+                                            }
+                                        })(ap, pd, item);
+                                }
+                            });   
                         }
-                    });   
-                }
-                
-                //console.log(err);
-            });
-        }catch (err){
-            //console.log(err);
-        }
-        
-    }
-    
-    function exception(item){
-        return false;
-        //return !item.match(/^[^.]/);
-        
-    }
-    
-    
-    function pad(n, width, z) {
-        z = z || '0';
-        n = n + '';
-        return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-    }
-    
-    function recordFile(o){
-        
-        
-        if(initRun){
-            console.log(`${pad('count',10, ' ')} ${pad('inode', 13, ' ')}    file`);
-            initRun = false;
-            (function(db){
-                var columns = "";
-                    columns += "file TEXT PRIMARY KEY, ";
-                    columns += "filename TEXT, ";
-                    columns += "path TEXT, ";
-                    columns += "dev INT, ";
-                    columns += "mode INT, ";
-                    columns += "nlink INT, ";
-                    columns += "uid INT, ";
-                    columns += "gid INT, ";
-                    columns += "rdev INT, ";
-                    columns += "blksize INT, ";
-                    columns += "inodeID INT, ";
-                    columns += "size INT, ";
-                    columns += "blocks INT, ";
-                    columns += "atimeMs REAL, ";
-                    columns += "mtimeMs REAL, ";
-                    columns += "ctimeMs REAL, ";
-                    columns += "birthtimeMs REAL, ";
-                    columns += "atime TEXT, ";
-                    columns += "mtime TEXT, ";
-                    columns += "ctime TEXT, ";
-                    columns += "birthtime TEXT";
 
-                var sql = `CREATE TABLE IF NOT EXISTS  files (${columns})`;
-                //console.log(sql);
-                var stmt = db.prepare(sql);
-                    stmt.run();
-            })(db);
-        }
-        
-        (function(o, db){
-            var columns = "";
-                columns += "file, ";
-                columns += "filename, ";
-                columns += "path, ";
-                columns += "dev, ";
-                columns += "mode, ";
-                columns += "nlink, ";
-                columns += "uid, ";
-                columns += "gid, ";
-                columns += "rdev, ";
-                columns += "blksize, ";
-                columns += "inodeID, ";
-                columns += "size, ";
-                columns += "blocks, ";
-                columns += "atimeMs, ";
-                columns += "mtimeMs, ";
-                columns += "ctimeMs, ";
-                columns += "birthtimeMs, ";
-                columns += "atime, ";
-                columns += "mtime, ";
-                columns += "ctime, ";
-                columns += "birthtime";
-            var values = "";
-                values += '$file, ';
-                values += '$filename, ';
-                values += '$path, ';//
-                values += '@dev, ';
-                values += '@mode, ';
-                values += '@nlink, ';
-                values += '@uid, ';
-                values += '@gid, ';
-                values += '@rdev, ';
-                values += '@blksize, ';
-                values += '@inodeID, ';
-                values += '@size, ';
-                values += '@blocks, ';
-                values += '@atimeMs, ';
-                values += '@mtimeMs, ';
-                values += '@ctimeMs, ';
-                values += '@birthtimeMs, ';
-                values += `"${o.atime}", `;
-                values += `"${o.mtime}", `;
-                values += `"${o.ctime}", `;
-                values += `"${o.birthtime}"`;
-                
-            var sql = `INSERT OR REPLACE INTO files (${columns}) VALUES (${values})`;
-            
-            //console.log(sql);
-            //console.log(o);
-            var stmt = db.prepare(sql);
-            //console.log(stmt);
-            stmt.run(o);
-            
-        })(o, db);
-        
-        processCount++;
-        console.log(`${pad(processCount,10, ' ')} ${pad(o.inodeID, 13, ' ')}   ${o.file}`);
-        //console.log(o);
-        //db.close();
+                        //console.log(err);
+                    });
+                }catch (err){
+                    //console.log(err);
+                }
+
+            }
+
+            function exception(item){
+                return false;
+                //return !item.match(/^[^.]/);
+
+            }
+
+
+            function pad(n, width, z) {
+                z = z || '0';
+                n = n + '';
+                return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+            }
+
+            function recordFile(o){
+
+
+                if(initRun){
+                    console.log(`${pad('count',10, ' ')} ${pad('inode', 13, ' ')}    file`);
+                    initRun = false;
+                    (function(db){
+                        var columns = "";
+                            columns += "file TEXT PRIMARY KEY, ";
+                            columns += "filename TEXT, ";
+                            columns += "path TEXT, ";
+                            columns += "dev INT, ";
+                            columns += "mode INT, ";
+                            columns += "nlink INT, ";
+                            columns += "uid INT, ";
+                            columns += "gid INT, ";
+                            columns += "rdev INT, ";
+                            columns += "blksize INT, ";
+                            columns += "inodeID INT, ";
+                            columns += "size INT, ";
+                            columns += "blocks INT, ";
+                            columns += "atimeMs REAL, ";
+                            columns += "mtimeMs REAL, ";
+                            columns += "ctimeMs REAL, ";
+                            columns += "birthtimeMs REAL, ";
+                            columns += "atime TEXT, ";
+                            columns += "mtime TEXT, ";
+                            columns += "ctime TEXT, ";
+                            columns += "birthtime TEXT";
+
+                        var sql = `CREATE TABLE IF NOT EXISTS  files (${columns})`;
+                        //console.log(sql);
+                        var stmt = db.prepare(sql);
+                            stmt.run();
+                    })(db);
+                }
+
+                (function(o, db){
+                    var columns = "";
+                        columns += "file, ";
+                        columns += "filename, ";
+                        columns += "path, ";
+                        columns += "dev, ";
+                        columns += "mode, ";
+                        columns += "nlink, ";
+                        columns += "uid, ";
+                        columns += "gid, ";
+                        columns += "rdev, ";
+                        columns += "blksize, ";
+                        columns += "inodeID, ";
+                        columns += "size, ";
+                        columns += "blocks, ";
+                        columns += "atimeMs, ";
+                        columns += "mtimeMs, ";
+                        columns += "ctimeMs, ";
+                        columns += "birthtimeMs, ";
+                        columns += "atime, ";
+                        columns += "mtime, ";
+                        columns += "ctime, ";
+                        columns += "birthtime";
+                    var values = "";
+                        values += '$file, ';
+                        values += '$filename, ';
+                        values += '$path, ';//
+                        values += '@dev, ';
+                        values += '@mode, ';
+                        values += '@nlink, ';
+                        values += '@uid, ';
+                        values += '@gid, ';
+                        values += '@rdev, ';
+                        values += '@blksize, ';
+                        values += '@inodeID, ';
+                        values += '@size, ';
+                        values += '@blocks, ';
+                        values += '@atimeMs, ';
+                        values += '@mtimeMs, ';
+                        values += '@ctimeMs, ';
+                        values += '@birthtimeMs, ';
+                        values += `"${o.atime}", `;
+                        values += `"${o.mtime}", `;
+                        values += `"${o.ctime}", `;
+                        values += `"${o.birthtime}"`;
+
+                    var sql = `INSERT OR REPLACE INTO files (${columns}) VALUES (${values})`;
+
+                    //console.log(sql);
+                    //console.log(o);
+                    var stmt = db.prepare(sql);
+                    //console.log(stmt);
+                    stmt.run(o);
+
+                })(o, db);
+
+                processCount++;
+                console.log(`${pad(processCount,10, ' ')} ${pad(o.inodeID, 13, ' ')}   ${o.file}`);
+                //console.log(o);
+                //db.close();
+            }
+
+            mapWalkPath(walkpath, function(o){
+                recordFile(o);
+            });
+
+            //console.log("\n\nfinished!");
+        })(arg1, arg2);
     }
     
-    mapWalkPath('/mnt/stronghold/', function(o){
-        recordFile(o);
-    });
-    
-    //console.log("\n\nfinished!");
 })();
+
--- hashfiles.js
+++ hashfiles.js
@@ -1,142 +1,176 @@
 (function(){
-    var fs = require('fs');
-    var p = require("path");
-    var hasha = require("hasha");
-    var sqlite3 = require('better-sqlite3');
     
-    var db = new sqlite3('files.sqlite');
+    var arg1 = process.argv[2];
+    //var arg2 = process.argv[3];
     
-    
-    var processCount = 0;
-    var chunkSize = 100;
-    var fileCount = 0;
-    var position = 0;
-    var chunk = [];
-    
-    function exception(item){
+    if(arg1 === '-h' || arg1 === '--help'){
+        var help = '';
         
-        return !item.match(/^[^.]/);
+        help += "\n";
+        help += "hashfiles is a file analysis tool\n\n";
         
-    }
-    
-    function hashFile(o, mapFunction){
-        /*
-        (function(filepath){
-            hasha.fromFile(filepath, {algorithm: 'sha256'}).then(hash => {
-                console.log(`file: ${filepath}, hash: ${hash}`);
-            });
-        })(filepath);//*/
-        o.hashAlgorithm = 'sha256';
-        o.hash = getHash(o.inodeID);
-        //*
-        if(o.hash){
-            mapFunction(o);
-        }else{
-            hasha.fromFile(o.file, {algorithm: o.hashAlgorithm}).then(hash => {
-                //console.log(`file: ${o.file}, hash: ${hash}`);
-                o.hash = hash;
+        help += "It will process an sqlite file created by getfiles.\n";
+        help += "hashfiles will calculate a hash for each file in the database,\n";
+        help += "then it will store the results in a 'hashes' table.\n\n";
+        
+        help += "NOTE: \n";
+        help += " - The file's inodeID is used as the primary key for the hash table.\n";
+        help += " - At present this tool is configured to use sha256 as the hash algorithm.\n\n";
+        
+        help += "The first argument is the name of the sqlite database to process.\n\n";
+                
+        help += "Usage example: node hashfiles.js test.sqlite\n";
+        
+        
+        console.log(help);
+    }else if(arg1){
+        (function(dbname){
+            var fs = require('fs');
+            var p = require("path");
+            var hasha = require("hasha");
+            var sqlite3 = require('better-sqlite3');
 
-                mapFunction(o);
-            });//*/
-        }
-        
-    }
-    
-    function pad(n, width, z) {
-        z = z || '0';
-        n = n + '';
-        return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-    }
-    
-    function getfiles(start, end){
-        //select rowid, file, inodeID From files  where rowid > 5 and rowid < 10
-        var sql = `select file, inodeID From files  where rowid >= ${start} and rowid <= ${end}`;
-        //console.log(sql);
-        var files = db.prepare(sql).all();
-            
-        return files;        
-    }
-    function getHash(inodeID){
-        //select rowid, file, inodeID From files  where rowid > 5 and rowid < 10
-        var sql = `select hash From hashes  where inodeID = ${inodeID}`;
-        //console.log(sql);
-        return db.prepare(sql).get().hash;
-               
-    }
-    
-    function getFileCount(){
-        //select rowid, file, inodeID From files  where rowid > 5 and rowid < 10
-        var sql = 'select rowid from files order by rowid desc limit 1';
-        //console.log(sql);
-        var o = db.prepare(sql).get();
-        
-        return o.rowid;
-    }
-    
-    function createHashTable(){
-        console.log(`${pad('count',10, ' ')} ${pad('inode', 13, ' ')} ${pad('hash', 68, ' ')}     file`);
-        
-        var columns = "";
-            columns += "inodeID int PRIMARY KEY, ";
-            columns += "hashAlgorithm TEXT, ";
-            columns += "hash TEXT ";
+            var db = new sqlite3(dbname);
 
-        var sql = `CREATE TABLE IF NOT EXISTS  hashes (${columns})`;
-        //console.log(sql);
-        var stmt = db.prepare(sql);
-            stmt.run();
-        
-    }
-    
-    function recordHash(o, callback){
-        callback = callback || function(){};
-        
-        var columns = "";
-            columns += "inodeID, ";
-            columns += "hashAlgorithm, ";
-            columns += "hash";
-        var values = "";
-            values += '@inodeID, ';
-            values += '$hashAlgorithm, ';
-            values += '$hash';
-        var sql = `INSERT OR REPLACE INTO hashes (${columns}) VALUES (${values})`;
 
-        //console.log(sql);
-        //console.log(o);
-        var stmt = db.prepare(sql);
-        //console.log(stmt);
-        stmt.run(o);
-        
-        processCount++;
-        console.log(`${pad(processCount,10, ' ')} ${pad(o.inodeID, 13, ' ')} ${pad(o.hash, 68, ' ')}     ${o.file}`);
-        callback();
+            var processCount = 0;
+            var chunkSize = 100;
+            var fileCount = 0;
+            var position = 0;
+            var chunk = [];
+
+            function exception(item){
+
+                return !item.match(/^[^.]/);
+
+            }
+
+            function hashFile(o, mapFunction){
+                /*
+                (function(filepath){
+                    hasha.fromFile(filepath, {algorithm: 'sha256'}).then(hash => {
+                        console.log(`file: ${filepath}, hash: ${hash}`);
+                    });
+                })(filepath);//*/
+                o.hashAlgorithm = 'sha256';
+                o.hash = '';
+                var temp = getHash(o.inodeID);
+                //*
+                if(temp){
+                    o.hash = temp.hash;
+                    mapFunction(o);
+                }else{
+                    hasha.fromFile(o.file, {algorithm: o.hashAlgorithm}).then(hash => {
+                        //console.log(`file: ${o.file}, hash: ${hash}`);
+                        o.hash = hash;
+
+                        mapFunction(o);
+                    });//*/
+                }
+
+            }
+
+            function pad(n, width, z) {
+                z = z || '0';
+                n = n + '';
+                return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+            }
+
+            function getfiles(start, end){
+                //select rowid, file, inodeID From files  where rowid > 5 and rowid < 10
+                var sql = `select file, inodeID From files  where rowid >= ${start} and rowid <= ${end}`;
+                //console.log(sql);
+                var files = db.prepare(sql).all();
+
+                return files;        
+            }
+            function getHash(inodeID){
+                //select rowid, file, inodeID From files  where rowid > 5 and rowid < 10
+                var sql = `select hash From hashes  where inodeID = ${inodeID}`;
+                //console.log(sql);
+                return db.prepare(sql).get();
+
+            }
+
+            function getFileCount(){
+                //select rowid, file, inodeID From files  where rowid > 5 and rowid < 10
+                var sql = 'select rowid from files order by rowid desc limit 1';
+                //console.log(sql);
+                var o = db.prepare(sql).get();
+
+                return o.rowid;
+            }
+
+            function createHashTable(){
+                console.log(`${pad('count',10, ' ')} ${pad('inode', 13, ' ')} ${pad('hash', 68, ' ')}     file`);
+
+                var columns = "";
+                    columns += "inodeID int PRIMARY KEY, ";
+                    columns += "hashAlgorithm TEXT, ";
+                    columns += "hash TEXT ";
+
+                var sql = `CREATE TABLE IF NOT EXISTS  hashes (${columns})`;
+                //console.log(sql);
+                var stmt = db.prepare(sql);
+                    stmt.run();
+
+            }
+
+            function recordHash(o, callback){
+                callback = callback || function(){};
+
+                var columns = "";
+                    columns += "inodeID, ";
+                    columns += "hashAlgorithm, ";
+                    columns += "hash";
+                var values = "";
+                    values += '@inodeID, ';
+                    values += '$hashAlgorithm, ';
+                    values += '$hash';
+                var sql = `INSERT OR REPLACE INTO hashes (${columns}) VALUES (${values})`;
+
+                //console.log(sql);
+                //console.log(o);
+                var stmt = db.prepare(sql);
+                //console.log(stmt);
+                stmt.run(o);
+
+                processCount++;
+                console.log(`${pad(processCount,10, ' ')} ${pad(o.inodeID, 13, ' ')} ${pad(o.hash, 68, ' ')}     ${o.file}`);
+                callback();
+            }
+            function stackFreeCall(callback){
+                setTimeout(callback, 1);
+            }
+
+            function eatChunk(){
+                if(chunk.length){
+                    hashFile(chunk.pop(), function(o){
+                        recordHash(o, stackFreeCall(eatChunk));
+                    });
+                }else if(position < fileCount){
+                    chunk = getfiles(position, position + chunkSize);
+                    position += chunkSize;
+                    stackFreeCall(eatChunk);
+                }else{
+                    console.log("Done!");
+                }
+            }
+
+            function main(){
+                createHashTable();
+                chunkSize = 100;
+                fileCount = getFileCount();
+                eatChunk();
+            }
+
+            main();
+
+
+        })(arg1);
     }
-    function stackFreeCall(callback){
-        setTimeout(callback, 1);
-    }
-    
-    function eatChunk(){
-        if(chunk.length){
-            hashFile(chunk.pop(), function(o){
-                recordHash(o, stackFreeCall(eatChunk));
-            });
-        }else if(position < fileCount){
-            chunk = getfiles(position, position + chunkSize);
-            position += chunkSize;
-            stackFreeCall(eatChunk);
-        }else{
-            console.log("Done!");
-        }
-    }
-    
-    function main(){
-        createHashTable();
-        chunkSize = 100;
-        fileCount = getFileCount();
-        eatChunk();
-    }
-    
-    main();
-    
     
 })();
+
+
+
